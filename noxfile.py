640600715c33286110793d379842f3ee9da8fd53
rbpatt2019 <rb.patterson.cross@gmail.com>
2020-12-23 14:40:48 +0000

    Add black support

--- noxfile.py
+++ noxfile.py
@@ -1,30 +1,57 @@
 import nox
 import tempfile
 
-LOCATIONS=["src", "tests", "noxfile.py"]
-VERSIONS=["3.9", "3.8", "3.7"]
+LOCATIONS = ["src", "tests", "noxfile.py"]
+VERSIONS = ["3.9", "3.8", "3.7"]
+
 
 def constrained_install(session, *args, **kwargs):
     with tempfile.NamedTemporaryFile() as reqs:
-        session.run('poetry', 'export', '--dev', '--without-hashes', '--format=requirements.txt', f'--output={reqs.name}', external=True)
-        session.install(f'--constraint={reqs.name}', *args, **kwargs)
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--without-hashes",
+            "--format=requirements.txt",
+            f"--output={reqs.name}",
+            external=True,
+        )
+        session.install(f"--constraint={reqs.name}", *args, **kwargs)
+
+
+@nox.session(python="3.9")
+def format(session):
+    args = session.posargs or LOCATIONS
+    constrained_install(session, "black")
+    session.run("black", *args)
+
 
 @nox.session(python=VERSIONS)
 def lint(session):
     # As pylint requires installed packages and nox spins off isolated venvs,
-    # Import error is silenced. This is checked by pyright, and helps 
+    # Import error is silenced. This is checked by pyright, and helps
     # prevent lengthy installs
     args = session.posargs or LOCATIONS
-    constrained_install(session, 'pylint')
-    session.run('pylint', "--disable=import-error", *args)
+    constrained_install(session, "pylint")
+    session.run("pylint", "--disable=import-error", *args)
+
 
 @nox.session(python=VERSIONS)
 def tests(session):
     args = session.posargs or []
 
     # Install app requirements generally
-    session.run("poetry", "install", '--no-dev', external=True)
+    session.run("poetry", "install", "--no-dev", external=True)
     # Install testing requirements explicitly
-    constrained_install(session, 'coverage', 'pytest', 'pytest-clarity', 'pytest-sugar', 'pytest-mock', 'pytest-cov', 'six')
+    constrained_install(
+        session,
+        "coverage",
+        "pytest",
+        "pytest-clarity",
+        "pytest-sugar",
+        "pytest-mock",
+        "pytest-cov",
+        "six",
+    )
 
     session.run("pytest", *args)
