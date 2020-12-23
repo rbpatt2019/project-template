9a3469d9a8200356cc4884f51bf970c67bc35a9e
rbpatt2019 <rb.patterson.cross@gmail.com>
2020-12-23 11:12:31 +0000

    Initialise basic script

---
+++ src/project_template/console.py
@@ -0,0 +1,12 @@
+from . import __version__ 
+
+def main(version):
+    """TODO: Docstring for main.
+
+    :function: TODO
+    :returns: TODO
+
+    """
+    print(f"Project is version {version}")
+
+
