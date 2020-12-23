8f955267ff183855dbf89af81d7c52021fc7a2c9
rbpatt2019 <rb.patterson.cross@gmail.com>
2020-12-23 14:07:04 +0000

    Add pylint support

--- noxfile.py
+++ noxfile.py
@@ -1,12 +1,21 @@
 import nox
 import tempfile
 
+LOCATIONS=["src", "tests", "noxfile.py"]
+VERSIONS=["3.9", "3.8", "3.7"]
+
 def constrained_install(session, *args, **kwargs):
     with tempfile.NamedTemporaryFile() as reqs:
         session.run('poetry', 'export', '--dev', '--format=requirements.txt', f'--output={regs.name}', external=True)
         session.install(f'--constraint={regs.name}', *args, **kwargs)
 
-@nox.session(python=["3.9", "3.8", "3.7"])
+@nox.session(python=VERSIONS)
+def lint(session):
+    args = session.posargs or LOCATIONS
+    constrained_install(session, 'pylint')
+    session.run('pylint', *args)
+
+@nox.session(python=VERSIONS)
 def tests(session):
     args = session.posargs or []
 
