b7f51e74f99b59acb3e2747e13302fa2078bf55c
rbpatt2019 <rb.patterson.cross@gmail.com>
2020-12-23 13:53:51 +0000

    Add constrained_install for nox

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,10 @@
 import nox
+import tempfile
+
+def constrained_install(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as reqs:
+        session.run('poetry', 'export', '--dev', '--format=requirements.txt', f'--output={regs.name}', external=True)
+        session.install(f'--constraint={regs.name}', *args, **kwargs)
 
 @nox.session(python=["3.9", "3.8", "3.7"])
 def tests(session):
