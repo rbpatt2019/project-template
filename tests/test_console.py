781884134f644f2809da23efdbc2ee588467c631
rbpatt2019 <rb.patterson.cross@gmail.com>
2020-12-23 12:35:51 +0000

    Add initial test suite

---
+++ tests/test_console.py
@@ -0,0 +1,23 @@
+from project_template import console, __version__
+import pytest
+
+@pytest.fixture
+def mock_print(mocker):
+    return mocker.patch("builtins.print")
+
+def test_main_called(mock_print):
+    """It calls the function"""
+    console.main()
+    assert mock_print.called
+
+def test_main_args(mock_print):
+    """It is called with the current version"""
+    console.main()
+    args, _ = mock_print.call_args
+    assert f"Project is version {__version__}" in args[0]
+
+def test_main_exception(mock_print):
+    '''It fails as expected'''
+    mock_print.side_effect = Exception("This is forced!")
+    with pytest.raises(Exception):
+        console.main()
