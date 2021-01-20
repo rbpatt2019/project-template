97ae899ed77d2d12d82389a55ce8b1e6ef3b2c60
rbpatt2019 <rb.patterson.cross@gmail.com>
2021-01-20 16:31:59 +0000

    Linting

--- noxfile.py
+++ noxfile.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
-import multiprocessing
+"""Nox session configuration."""
 import tempfile
 from typing import Any, List
 
-# pylint: disable=import-error
-import nox  # type: ignore
-from nox.sessions import Session  # type: ignore
+import nox
+from nox.sessions import Session
 
-LOCATIONS: List[str] = ["src", "tests", "noxfile.py"]
+LOCATIONS: List[str] = [
+    "project_template",
+    "noxfile.py",
+    "tests",
+]
 PACKAGE: str = "project_template"
 VERSIONS: List[str] = ["3.9", "3.8", "3.7"]
-CORES: int = int(multiprocessing.cpu_count() / 2)
 
 nox.options.stop_on_first_error = True
 nox.options.reuse_existing_virtualenvs = True
@@ -18,7 +20,7 @@ nox.options.reuse_existing_virtualenvs = True
 # There's a good chance that this will get refactored out if I move to pyup
 # which requires a requirements.txt file
 def constrained_install(session: Session, *args: str, **kwargs: Any) -> None:
-    """Install packages with poetry version constraint"""
+    """Install packages with poetry version constraint."""
     with tempfile.NamedTemporaryFile() as reqs:
         session.run(
             "poetry",
@@ -34,7 +36,7 @@ def constrained_install(session: Session, *args: str, **kwargs: Any) -> None:
 
 @nox.session(python="3.9")
 def form(session: Session) -> None:
-    """Format code with isort and black"""
+    """Format code with isort and black."""
     args = session.posargs or LOCATIONS
     constrained_install(session, "isort", "black")
     session.run("isort", *args)
@@ -43,11 +45,8 @@ def form(session: Session) -> None:
 
 @nox.session(python=VERSIONS)
 def security(session: Session) -> None:
-    """Check security with bandit and safety"""
-    # I'd like to move skip to config file, but bandit doesn't yet support pyproject
-    args = session.posargs or LOCATIONS
-    constrained_install(session, "safety", "bandit")
-    session.run("bandit", "--skip=B101", "-r", *args)
+    """Check security safety."""
+    constrained_install(session, "safety")
     with tempfile.NamedTemporaryFile() as reqs:
         session.run(
             "poetry",
@@ -63,21 +62,27 @@ def security(session: Session) -> None:
 
 @nox.session(python=VERSIONS)
 def lint(session: Session) -> None:
-    """Lint files with pylint and pyright"""
-    args = session.posargs or LOCATIONS
-    session.run("poetry", "install", "--no-dev", external=True)  # To check imports
-    # Pytest required to prevent error
-    constrained_install(session, "pylint", "darglint", "pytest", "pytest-mock")
-    session.run("pylint", f"-j {CORES}", *args)
-    session.run("pyright", *args, external=True)  # I'd prefer a local install...
-    session.run(
-        "darglint", "-s", "numpy", "-z", "short", "-v", "2", "-l", "DEBUG", *args
+    """Lint files with flake8 and pyright."""
+    args = session.posargs or LOCATIONS[:2]
+    constrained_install(
+        session,
+        "flakehell",
+        "flake8-annotations",
+        "flake8-bugbear",
+        "flake8-bandit",
+        "flake8-docstrings",
+        "flake8-pytest-style",
+        "darglint",
+        "nox",  # Keeps pyright happy
     )
+    session.run("flakehell", "lint", *args)
+    # I'd prefer a local install...
+    session.run("poetry", "run", "pyright", *args, external=True)
 
 
 @nox.session(python=VERSIONS)
 def tests(session: Session) -> None:
-    """Run the test suite with pytest"""
+    """Run the test suite with pytest."""
     args = session.posargs or []
     session.run("poetry", "install", "--no-dev", external=True)
     constrained_install(  # These are required for tests. Don't clutter w/ all dev deps!
@@ -91,12 +96,13 @@ def tests(session: Session) -> None:
         "typeguard",
         "six",
     )
+    session.run("poetry", "run", "pyright", LOCATIONS[2])
     session.run("pytest", *args)
 
 
 @nox.session(python=VERSIONS)
 def doc_tests(session: Session) -> None:
-    """Test docstrings with xdoctest"""
+    """Test docstrings with xdoctest."""
     args = session.posargs or ["all"]
     session.run("poetry", "install", "--no-dev", external=True)
     constrained_install(session, "xdoctest")
@@ -108,6 +114,7 @@ def doc_tests(session: Session) -> None:
         "2",
         "--report",
         "cdiff",
+        "--nocolor",
         PACKAGE,
         *args,
     )
@@ -115,7 +122,7 @@ def doc_tests(session: Session) -> None:
 
 @nox.session(python="3.9")
 def doc_build(session: Session) -> None:
-    """Build the documentation"""
+    """Build the documentation."""
     session.run("poetry", "install", "--no-dev", external=True)
     constrained_install(session, "sphinx", "sphinx-rtd-theme")
     session.run("sphinx-build", "docs", "docs/_build")
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
+"""Pytest configuration."""
 from unittest.mock import Mock
 
 import pytest
 from pytest_mock import MockerFixture
 
 
-@pytest.fixture
+@pytest.fixture()
 def mock_print(mocker: MockerFixture) -> Mock:
+    """Mock the builtin print function."""
     return mocker.patch("builtins.print")
